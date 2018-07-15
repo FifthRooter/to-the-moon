7f6ffda3a1018b27531962d50ded51849874f12e
LAPTOP-06QAOFOM\jelle <jelle.slief@hotmail.com>
2018-07-15 15:04:33 +0200

    [Ädded function do_physics() and a global bool success to check for mission success/failure]

--- to-the-moon/Physics.cpp
+++ to-the-moon/Physics.cpp
@@ -1,19 +1,59 @@
 #include "stdafx.h"
 #include "Physics.h"
+#include "Dashboard.h"
+#include <fstream>
+#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-Physics::Physics()
-{
-}
+bool success = 0;
 
-Physics::~Physics()
-{
-}
+vector< state_type> do_physics( double angle) {
 
-float Physics::getEarthMoonDistance()
-{
-	return distanceEarthSC;
-}
+	// Write output to file
+	ofstream outfile;
+	outfile.open("file.txt", ofstream::out);
 
-Vector2f Physics::getSCCoordinates() {
-	return scCoordinates;
+	// State initialization
+	state_type x;
+	x[0] = -0.0017295;			// Moon initial x-position, in AU
+	x[1] = 0.00043495;			// Moon initial x-velocity, in AU/day
+	x[2] = -0.0018849;			// Moon initial y-position, in AU
+	x[3] = -0.0003173;			// Moon initial y-velocity, in AU/day
+	x[4] = -3.0144939e-5;		// Spacecraft initial x-position, in AU
+	x[5] = -v * sin(angle);		// Spacecraft initial x-velocity, in AU/day
+	x[6] = -3.2851355e-5;		// Spacecraft initial y-position, in AU
+	x[7] = -v * cos(angle);		// Spacecraft initial y-velocity, in AU/day
+
+
+	System sys(G, m_E, m_M, m_SC);
+	vector< state_type > x_vec;
+	vector< double > times;
+
+	// Do the simulation
+	runge_kutta4< state_type > stepper;
+	size_t steps = integrate_const(stepper, sys, x, 0.0, 1.0, 0.0007, push_back_state_and_time(x_vec, times));
+
+	// Desired distance from moon
+	double range = 1.29480452e-5;
+
+	// Write the results of the simulation to the external file, and check whether the spacecraft reached the moon
+	for (size_t i = 0; i <= steps; i++)
+	{
+		if (sqrt(pow(x_vec[i][0] - x_vec[i][4], 2) + pow(x_vec[i][2] - x_vec[i][6], 2)) <= range)
+		{
+			bool success = 1;
+			break;
+		}
+		outfile << to_string(times[i]);
+		for (size_t j = 0; j < dims; j++)
+		{
+			outfile << ',' << to_string(x_vec[i][j]);
+		}
+		outfile << ',' << to_string(sqrt(pow(x_vec[i][0] - x_vec[i][4], 2) + pow(x_vec[i][2] - x_vec[i][6], 2))) << '\n';
+
+	}
+
+	// Return the Moon and spacecraft coordinates from the simulation
+	return x_vec;
 }
--- to-the-moon/Physics.h
+++ to-the-moon/Physics.h
@@ -1,52 +1,62 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#define _USE_MATH_DEFINES
+#include "stdafx.h"
 #include <cmath>
+#define _USE_MATH_DEFINES
 #include <math.h>
+#include <boost/numeric/odeint.hpp>
+#include <vector>
+#include <boost/array.hpp>
 
-using namespace sf;
+using namespace std;
+using namespace boost::numeric::odeint;
 
-class Physics
-{
+const size_t dims = 8;						// Number of variables in the state
+
+typedef array<double, dims> state_type;		// Array of size dims to store state
+
+const double G = 2.95912208286e-4;			// Gravitational constant in km^3 solar_mass^-1 day^-1
+const double m_E = 3.0035e-6;				// Earth mass in solar masses
+const double m_M = 3.6923e-8;				// Moon mass in solar masses
+const double m_SC = 1.1064e-27;				// Spacecraft total mass in solar masses
+const double v = 0.007093217471;			// Spacecraft launch velocity in AU/day
+extern bool success;						// To indicate whether the moon was reached
+
+class System {
+
+	double m_G;		// Gravitational constant
+	double m_M_E;	// Earth mass
+	double m_M_M;	// Moon mass
+	double m_M_SC;	// Spacecraft mass
+
 public:
-	Physics();
-	~Physics();
-	float getEarthMoonDistance();
-	Vector2f getSCCoordinates();
+	System(double G, double m_E, double m_M, double m_SC) : m_G(G), m_M_E(m_E), m_M_M(m_M), m_M_SC(m_SC) { }
 
-private:
-
-	double angle = M_PI;
-	int R_PERIGEE; // Or SC_LEO_ALT
-	int R_APOGEE; // Or MOON_EARTH_DISTANCE
-	int GRAVIT_ACCEL = 9.80665; // gravitational acceleration
-	int I_Sp; // Specific impulse
-
-	float eccentricity = (R_PERIGEE - R_APOGEE) / (R_PERIGEE + R_APOGEE);
-	float velocityCp = sqrtf(MU_EARTH / R_PERIGEE);
-	float deltaVp = velocityCp * (sqrtf(1+eccentricity) - 1) ; // to enter E-M Hohmann transfer orbit, this change in velocity required
-	float MU_EARTH = 3.98600441*1e+14; 
-	float MU_MOON = 4.9048695*1e+12; 
-	float distanceEarthSC = 384400; //km
-
-	float massDry;
-	float massProp;
-	float massTotal = massDry + massProp;
-	float deltaV = I_Sp * GRAVIT_ACCEL * log(massTotal / massDry);
-
-	Vector2f forceEarthOnSC;
-	Vector2f forceMoonOnSC;
-
-	Vector2i earthCoordinates;
-
-	float thetaMoon;
-	float thetaMoonPrevious;
-	Vector2i moonCoordinates;
-	Vector2i moonCoordinatePrevious;
-	Vector2i deltaMoonCoordinates;
-
-	Vector2f scCoordinates;
-	Vector2f scCoordinatesPrevious;
-	Vector2i deltaSCCoordinates;
+	void operator() (const state_type &x, state_type &dxdt, const double /* t */)
+	{
+		dxdt[0] = x[1];
+		dxdt[1] = -m_G * m_M_E * x[0] / pow((pow(x[0], 2) + pow(x[2], 2)), 1.5);
+		dxdt[2] = x[3];
+		dxdt[3] = -m_G * m_M_E * x[2] / pow((pow(x[0], 2) + pow(x[2], 2)), 1.5);
+		dxdt[4] = x[5];
+		dxdt[5] = -m_G * m_M_E * x[4] / pow((pow(x[4], 2) + pow(x[6], 2)), 1.5) - m_G * m_M_M * (x[4] - x[0]) / pow(pow(x[4] - x[0], 2) + pow(x[6] - x[4], 2), 1.5);
+		dxdt[6] = x[7];
+		dxdt[7] = -m_G * m_M_E * x[6] / pow((pow(x[4], 2) + pow(x[6], 2)), 1.5) - m_G * m_M_M * (x[6] - x[2]) / pow(pow(x[4] - x[0], 2) + pow(x[6] - x[4], 2), 1.5);
+	}
 };
 
+// Observer
+struct push_back_state_and_time
+{
+	vector< state_type >& m_states;
+	vector< double >& m_times;
+
+	push_back_state_and_time(vector< state_type > &states, vector< double > &times)
+		: m_states(states), m_times(times) { }
+
+	void operator()(const state_type &x, double t)
+	{
+		m_states.push_back(x);
+		m_times.push_back(t);
+	}
+};
+
+vector< state_type> do_physics( double );
